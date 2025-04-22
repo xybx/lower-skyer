e17efe868d2b4506780d8a1336e6d7cd7fb72835
Davie Jose <1239045617@qq.com>
2025-04-22 11:52:41 +0800

    Delete lower directory

--- lower/src/stores/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-import { createPinia } from 'pinia';
-const pinia = createPinia()
-
-export function setupStore(app: App<Element>) {
-    app.use(pinia)
-}
-
-export default pinia
